c140b7d6e52a0f88c8f989e035dabb1e40fabb54
Artem Shchygel <artems@ami.com>
2018-03-07 16:24:57 -0500

    add color script

---
+++ color.sh
@@ -0,0 +1,11 @@
+!#/bin/sh
+
+echo -e "\033[0;30mBLACK"       "\033[1;30mBRIGHT_BLACK"
+echo -e "\033[0;31mRED"         "\033[1;31mBRIGHT_RED"
+echo -e "\033[0;32mGREEN"       "\033[1;32mBRIGHT_GREEN"
+echo -e "\033[0;33mYELLOW"      "\033[1;33mBRIGHT_YELLOW"
+echo -e "\033[0;34mBLUE"        "\033[1;34mBRIGHT_BLUE"
+echo -e "\033[0;35mMAGENTA"     "\033[1;35mBRIGHT_MAGENTA"
+echo -e "\033[0;36mCYAN"        "\033[1;36mBRIGHT_CYAN"
+echo -e "\033[0;37mWHITE"       "\033[1;37mBRIGHT_WHITE"
+echo -e "\033[0;m"
